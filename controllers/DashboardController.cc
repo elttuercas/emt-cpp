9426a8e696ccc17f0553da552289595d3d377636
Carlos Amores <eltu@dmg-inc.com>
2021-01-26 10:56:01 +0100

    - Added logged in/logged out logic to nav view - Declared sample events vector globally in DashboardController - Added style for login button - Added flair

--- controllers/DashboardController.cc
+++ controllers/DashboardController.cc
@@ -1,44 +1,56 @@
+/*
+ * __/\\\\\\\\\\\\\\\__/\\\\____________/\\\\__/\\\\\\\\\\\\\\\_
+ *  _\/\\\///////////__\/\\\\\\________/\\\\\\_\///////\\\/////__
+ *   _\/\\\_____________\/\\\//\\\____/\\\//\\\_______\/\\\_______
+ *    _\/\\\\\\\\\\\_____\/\\\\///\\\/\\\/_\/\\\_______\/\\\_______
+ *     _\/\\\///////______\/\\\__\///\\\/___\/\\\_______\/\\\_______
+ *	    _\/\\\_____________\/\\\____\///_____\/\\\_______\/\\\_______
+ *	     _\/\\\_____________\/\\\_____________\/\\\_______\/\\\_______
+ *        _\/\\\\\\\\\\\\\\\_\/\\\_____________\/\\\_______\/\\\_______
+ *         _\///////////////__\///______________\///________\///________
+ */
+
 #include "DashboardController.h"
+
+std::vector<std::map<std::string, std::string>> rgSampleEvents = {
+        {
+                {"id", "1"},
+                {"host", "Silent"},
+                {"name", "[L2] Rust Wipe"},
+                {"platform", "Discord"},
+                {"rate", "0.0028 REP/s"},
+                {"startTime", "2021-01-25 23:05 CET"}
+        },
+        {
+                {"id", "2"},
+                {"host", "Eltu"},
+                {"name", "[DevOps] Test Event"},
+                {"platform", "Discord"},
+                {"rate", "0.0028 REP/s"},
+                {"startTime", "2021-01-24 21:09 CET"}
+        },
+        {
+                {"id", "3"},
+                {"host", "DonutKnight"},
+                {"name", "[SMILE] Apex Slave"},
+                {"platform", "Discord"},
+                {"rate", "0.0028 REP/s"},
+                {"startTime", "2019-05-24 22:05 CET"}
+        }
+};
 
 void DashboardController::get(const HttpRequestPtr &request, std::function<void(const HttpResponsePtr &)> &&callback)
 {
-    // Sample vector of events to be displayed.
-    std::vector<std::map<std::string, std::string>> rgEvents = {
-            {
-                    {"id", "1"},
-                    {"host", "Silent"},
-                    {"name", "[L2] Rust Wipe"},
-                    {"platform", "Discord"},
-                    {"rate", "0.0028 REP/s"},
-                    {"startTime", "2021-01-25 23:05 CET"}
-            },
-            {
-                    {"id", "2"},
-                    {"host", "Eltu"},
-                    {"name", "[DevOps] Test Event"},
-                    {"platform", "Discord"},
-                    {"rate", "0.0028 REP/s"},
-                    {"startTime", "2021-01-24 21:09 CET"}
-            },
-            {
-                    {"id", "3"},
-                    {"host", "DonutKnight"},
-                    {"name", "[SMILE] Apex Slave"},
-                    {"platform", "Discord"},
-                    {"rate", "0.0028 REP/s"},
-                    {"startTime", "2019-05-24 22:05 CET"}
-            }
-    };
-
     // Keep a maximum of 25 events in the vector.
-    if (rgEvents.size() > 25)
+    if (rgSampleEvents.size() > 25)
     {
-        rgEvents.resize(25);
+        rgSampleEvents.resize(25);
     }
 
     // Data object to be fed to the view.
     drogon::HttpViewData data;
-    data.insert("events", rgEvents);
+    data.insert("events", rgSampleEvents);
+    data.insert("loggedIn", false);
 
     drogon::HttpResponsePtr pResponse = drogon::HttpResponse::newHttpViewResponse("./views/dashboard.csp", data);
     callback(pResponse);
@@ -47,34 +59,6 @@ void DashboardController::get(const HttpRequestPtr &request, std::function<void(
 void DashboardController::getPaginated(const HttpRequestPtr &request, std::function<void(const HttpResponsePtr &)> &&callback,
                                        int page)
 {
-    // Sample vector of events to be displayed.
-    std::vector<std::map<std::string, std::string>> rgEvents = {
-            {
-                    {"id", "1"},
-                    {"host", "Silent"},
-                    {"name", "[L2] Rust Wipe"},
-                    {"platform", "Discord"},
-                    {"rate", "0.0028 REP/s"},
-                    {"startTime", "2021-01-25 23:05 CET"}
-            },
-            {
-                    {"id", "2"},
-                    {"host", "Eltu"},
-                    {"name", "[DevOps] Test Event"},
-                    {"platform", "Discord"},
-                    {"rate", "0.0028 REP/s"},
-                    {"startTime", "2021-01-24 21:09 CET"}
-            },
-            {
-                    {"id", "3"},
-                    {"host", "DonutKnight"},
-                    {"name", "[SMILE] Apex Slave"},
-                    {"platform", "Discord"},
-                    {"rate", "0.0028 REP/s"},
-                    {"startTime", "2019-05-24 22:05 CET"}
-            }
-    };
-
     if (page < 1)
     {
         /*
@@ -85,13 +69,13 @@ void DashboardController::getPaginated(const HttpRequestPtr &request, std::funct
     }
 
     const int iEventsToSlice = (page - 1) * 25;
-    if (rgEvents.size() < iEventsToSlice)
+    if (rgSampleEvents.size() < iEventsToSlice)
     {
         /*
          * Check if there are enough events to display something in the requested page. If there aren't, redirect
          * to the last possible page.
          */
-        const int               iLastPage = static_cast<int>(std::ceil(rgEvents.size() / 25)) ?: 1;
+        const int               iLastPage = static_cast<int>(std::ceil(rgSampleEvents.size() / 25)) ?: 1;
         drogon::HttpResponsePtr pResponse = drogon::HttpResponse::newRedirectionResponse(
                 "/dashboard/" + std::to_string(iLastPage) + '/'
         );
@@ -99,16 +83,17 @@ void DashboardController::getPaginated(const HttpRequestPtr &request, std::funct
         return;
     }
 
-    std::vector<decltype(rgEvents)::value_type>(rgEvents.begin() + iEventsToSlice, rgEvents.end()).swap(rgEvents);
+    std::vector<decltype(rgSampleEvents)::value_type>(rgSampleEvents.begin() + iEventsToSlice, rgSampleEvents.end()).swap(rgSampleEvents);
 
     // From this point forth, the process is the same as when displaying the first 25 events by default.
-    if (rgEvents.size() > 25)
+    if (rgSampleEvents.size() > 25)
     {
-        rgEvents.resize(25);
+        rgSampleEvents.resize(25);
     }
 
     drogon::HttpViewData data;
-    data.insert("events", rgEvents);
+    data.insert("events", rgSampleEvents);
+    data.insert("loggedIn", true);
 
     drogon::HttpResponsePtr pResponse = drogon::HttpResponse::newHttpViewResponse("./views/dashboard.csp", data);
     callback(pResponse);
